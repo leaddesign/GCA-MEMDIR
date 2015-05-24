70a46c8bddedff4e7406d9f878bd7b5f647d8270
leaddesign <edcruz@3322622462789-623.local>
2015-05-23 19:15:23 -1000

    Initial Commit

---
+++ GCA-MEMDIR/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GCA-MEMDIR
+//
+//  Created by Ed Cruz on 5/23/15.
+//  Copyright (c) 2015 GuamContractorsAssociation. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
